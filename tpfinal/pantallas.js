0166e173306d91ed93c393d6a3621f590959995a
lucianotesta22 <83325082+lucianotesta22@users.noreply.github.com>
2024-10-10 16:01:46 -0300

    Update pantallas.js

--- tpfinal/pantallas.js
+++ tpfinal/pantallas.js
@@ -1,120 +1,180 @@
 // Definición de pantallas
 function pant1() {
+  x1 = 60;
+  x2 = 370;
+  y1 = 150;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 1', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant2() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 2', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant3() {
+  x1 = 160;
+  x2 = 370;
+  y1 = 110;
+  y2 = 280;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 3', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant4() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 4', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant5() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 5', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant6() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 6', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant7() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 7', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant8() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 8', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant9() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 9', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant10() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 10', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant11() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 11', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant12() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 12', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant13() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 13', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant14() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 14', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
 
 function pant15() {
+  x1 = 100;
+  x2 = 310;
+  y1 = 140;
+  y2 = 240;
   background(255);
   fill(0);
   textSize(32);
   text('Pantalla 15', width/2, 100);
-  drawButton();
+  drawButton(x1, y1, x2, y2);
 }
