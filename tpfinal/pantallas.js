e71443ec94423a8983ec12d87b52aad7f6684bd9
lucianotesta22 <83325082+lucianotesta22@users.noreply.github.com>
2024-10-10 17:38:59 -0300

    Update pantallas.js

--- tpfinal/pantallas.js
+++ tpfinal/pantallas.js
@@ -1,180 +1,189 @@
-// Definición de pantallas
-function pant1() {
-  x1 = 60;
-  x2 = 370;
-  y1 = 150;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 1', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant2() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 2', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant3() {
-  x1 = 160;
-  x2 = 370;
-  y1 = 110;
-  y2 = 280;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 3', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant4() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 4', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant5() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 5', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant6() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 6', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant7() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 7', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant8() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 8', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant9() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 9', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant10() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 10', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant11() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 11', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant12() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 12', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant13() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 13', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant14() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 14', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
-}
-
-function pant15() {
-  x1 = 100;
-  x2 = 310;
-  y1 = 140;
-  y2 = 240;
-  background(255);
-  fill(0);
-  textSize(32);
-  text('Pantalla 15', width/2, 100);
-  dibujarBoton(x1, y1, x2, y2);
+function pantallas() {
+  if (pantActual == 0) {
+    x1 = 60;
+    x2 = 370;
+    y1 = 150;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 1) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 2) {
+    x1 = 160;
+    x2 = 370;
+    y1 = 110;
+    y2 = 280;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 3) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 4) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 5) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 6) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 7) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 8) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 9) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 10) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 11) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 12) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 13) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 14) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+  if (pantActual == 15) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
+    if (pantActual == 16) {
+    x1 = 100;
+    x2 = 310;
+    y1 = 140;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    text(pant[pantActual], width/2, 100);
+    drawButton(x1, y1, x2, y2);
+  }
 }
