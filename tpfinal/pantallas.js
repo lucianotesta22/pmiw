07dded945df53642257e04235158d1b99fe9b4b2
lucianotesta22 <83325082+lucianotesta22@users.noreply.github.com>
2024-10-16 18:19:15 -0300

    Update pantallas.js

--- tpfinal/pantallas.js
+++ tpfinal/pantallas.js
@@ -1,5 +1,5 @@
 // Definición de pantallas
-function pantallas(texto) {
+function pantallas(texto,foto) {
     x1 = 60;
     x2 = 370;
     y1 = 150;
@@ -7,6 +7,7 @@ function pantallas(texto) {
     background(255);
     fill(0);
     textSize(32);
+    //image(foto
     text(texto, width/2, 100);
     dibujarBoton(x1, y1, x2, y2);
 }
