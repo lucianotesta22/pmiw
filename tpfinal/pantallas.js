4c436424ed921ab8d2f11d773f7c358b8e9f6456
lucianotesta22 <83325082+lucianotesta22@users.noreply.github.com>
2024-10-17 19:37:45 -0300

    Update pantallas.js

--- tpfinal/pantallas.js
+++ tpfinal/pantallas.js
@@ -1,5 +1,5 @@
-// Definición de pantallas
-function pantallas(texto,foto) {
+function pantalla(pantallaActual) {
+  if (pantSig[pantallaActual].length === 1) {
     x1 = 60;
     x2 = 370;
     y1 = 150;
@@ -7,7 +7,25 @@ function pantallas(texto,foto) {
     background(255);
     fill(0);
     textSize(32);
-    //image(foto[i],0,0,width,height);
-    text(texto, width/2, 100);
-    dibujarBoton(x1, y1, x2, y2);
+    image(img[pantallaActual], 0, 0, width, height);
+    dibujar1boton(x1, y1);
+    if (mouseIsPressed && dist(x1, y1, mouseX, mouseY) < 25) {
+      pantActual = pantSig[pantActual];
+    }
+  } else if (pantSig[pantallaActual].length === 2) {
+    x1 = 80;
+    x2 = 370;
+    y1 = 190;
+    y2 = 240;
+    background(255);
+    fill(0);
+    textSize(32);
+    image(img[pantallaActual], 0, 0, width, height);
+    dibujarBotones(x1, y1, x2, y2);
+    if (mouseIsPressed && dist(x1, y1, mouseX, mouseY) < 25) {
+      pantActual = pantActual + 1; 
+    } else if (mouseIsPressed && dist(x2, y2, mouseX, mouseY) < 25) {
+      pantActual = pantActual + 2;
+    }
+  }
 }
