df531f5f80e17cab4df084eb52bed8cc19d9f6e3
lucianotesta22 <83325082+lucianotesta22@users.noreply.github.com>
2024-10-17 19:37:05 -0300

    Update tpfinal.js

--- tpfinal/tpfinal.js
+++ tpfinal/tpfinal.js
@@ -1,13 +1,28 @@
-let pant = [];
-let imagenes = [];
+let textos = [];
+let img = [];
 let pantActual = 0;
 let x1, y1, x2, y2;
+let pantSig = {
+  0: [1],
+  1: [2],
+  2: [3, 4],
+  3 : [0],
+  4 : [5],
+  5 : [6, 7],
+  6 : [8],
+  7: [13],
+  8 : [9],
+  9 : [10],
+  10 : [11],
+  11 : [12],
+  12 : [14],
+  14 : [15],
+  };
 
 function preload() {
-  // Carga tanto de imagenes como de textos
-  for (var i = 0; i <= 16; i++) {
-    pant[i] = "pantalla " + i;
-  //imagenes[i] = loadImage("pantalla" + i + ".jpg);
+  for (var i = 0; i <= 14; i++) {
+    textos[i] = "pantalla " + i;
+    img[i] = loadImage("data/pantalla" + i + ".jpg");
   }
 }
 function setup() {
@@ -16,28 +31,17 @@ function setup() {
 }
 
 function draw() {
-  // Llama a la función de la pantalla actual
-  pantallas(pant[i],imagenes[i]);
+  // Llama a la función de las pantallas
+  pantalla(pantActual);
 }
-
-function mousePressed() {
-  // Cambiar a la siguiente pantalla al hacer clic
-  if (dist(mouseX, mouseY, x1, y1) < 25) {
-    pantActual = (pantActual + 1);
-  }
-  if (dist(mouseX, mouseY, x2, y2) < 25) {
-    pantActual = (pantActual + 2);
-  }
-  // cambiar a pantalla 0
-  if (dist(mouseX, mouseY, x1, y1) < 25 && pantActual >= 16) {
-    pantActual = 0;
-  }
-  if (dist(mouseX, mouseY, x2, y2) < 25 && pantActual >= 16) {
-    pantActual = 0;
-  }
+function dibujar1boton( x1, y1, x2, y2 ) {
+  fill(255);
+  ellipse(x1, y1, 50, 50);
+  ellipse(x2, y2, 50, 50);
+  textSize(16);
+  text('Siguiente', x1, y1 - 40);
 }
-
-function dibujarBoton( x1, y1, x2, y2 ) {
+function dibujarBotones( x1, y1, x2, y2 ) {
   fill(255);
   ellipse(x1, y1, 50, 50);
   ellipse(x2, y2, 50, 50);
@@ -46,4 +50,3 @@ function dibujarBoton( x1, y1, x2, y2 ) {
   text('Opcion 1', x1, y1 - 40);
   text('Opcion 2', x2, y2 - 40);
 }
-
